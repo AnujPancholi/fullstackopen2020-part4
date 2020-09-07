19affe241eb0378b07eb11e3137fedc449f5f986
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-07 22:43:12 +0530

    test: add few testcases for unit test of getTotalLikes helper function

--- app1-blog-app/src/tests/list_helper.test.js
+++ app1-blog-app/src/tests/list_helper.test.js
@@ -7,3 +7,54 @@ describe("Tests for dummy helper function",() => {
     expect(listHelpers.dummy([])).toBe(1);
   })
 })
+
+
+describe("Tests for getTotalLikes helper function",() => {
+
+  const TEST_CASES = [{
+    description: "getTotalLikes should return 0",
+    params: [
+      [
+                
+        {
+          "title": "Mock Blog Title",
+          "author": "Random Author",
+          "url": "https://www.mockblog.com/123456",
+          "likes": 0,
+          "id": "5f54c79917c0c7d1c608fca1"
+        },
+        {
+          "title": "Mock Blog Title 2",
+          "author": "Random Author",
+          "url": "https://www.mockblog.com/1234567",
+          "likes": 0,
+          "id": "5f54c9ed55a4c0d3ba73b10a"
+        }
+                
+      ]
+    ],
+    assert: function(targetFunction){
+      expect(targetFunction(...this.params)).toBe(0);
+    }
+  },{
+    description: "getTotalLikes should return 1",
+    params: [
+      [{
+        "title": "Mock Blog Title",
+        "author": "Random Author",
+        "url": "https://www.mockblog.com/123456",
+        "likes": 1,
+        "id": "5f54c79917c0c7d1c608fca1"
+      }]
+    ],
+    assert: function(targetFunction){
+      expect(targetFunction(...this.params)).toBe(1);
+    }
+  }]
+    
+  TEST_CASES.forEach((testCase) => {
+    test(testCase.description,() => {
+      testCase.assert(listHelpers.getTotalLikes);
+    })
+  })
+})
