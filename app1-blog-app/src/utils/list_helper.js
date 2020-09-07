8180c432eebdbc40425d75a39c79ae9018418a72
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-07 22:23:53 +0530

    feat: add getTotalLikes helper function

--- app1-blog-app/src/utils/list_helper.js
+++ app1-blog-app/src/utils/list_helper.js
@@ -2,6 +2,9 @@
 
 const dummy = (blogs) => 1;
 
+const getTotalLikes = (blogs) => blogs.reduce((__totalLikes,__blog) => __totalLikes+=__blog.likes,0);
+
 module.exports = {
-    dummy
+  dummy,
+  getTotalLikes
 }
