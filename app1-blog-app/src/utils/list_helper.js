537f576acb527ff41016742f0b1e4e3cdc0b29f7
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-08 19:12:36 +0530

    feat: add favoriteBlogs helper function

--- app1-blog-app/src/utils/list_helper.js
+++ app1-blog-app/src/utils/list_helper.js
@@ -4,7 +4,10 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => blogs.reduce((__totalLikes,__blog) => __totalLikes+=__blog.likes,0);
 
+const favoriteBlog = (blogs) => blogs.reduce((__favBlog,__blog) => __favBlog && __blog.likes>__favBlog.likes ? __blog : __favBlog, null);
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
