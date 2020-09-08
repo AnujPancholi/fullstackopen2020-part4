65b3dd7de78f82d840158aa55a0140bcc8850765
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-08 20:01:20 +0530

    feat: add mostBlogs helper function

--- app1-blog-app/src/utils/list_helper.js
+++ app1-blog-app/src/utils/list_helper.js
@@ -6,8 +6,25 @@ const totalLikes = (blogs) => blogs.reduce((__totalLikes,__blog) => __totalLikes
 
 const favoriteBlog = (blogs) => blogs.reduce((__favBlog,__blog) => !__favBlog || __blog.likes>__favBlog.likes ? __blog : __favBlog, null);
 
+const mostBlogs = (blogs) => {
+  const blogCountMap = new Map();
+  return blogs.reduce((mostBlogsObj,blog) => {
+    const authorCount = 1+(blogCountMap.get(blog.author) || 0);
+    blogCountMap.set(blog.author,authorCount);
+    if(authorCount>mostBlogsObj.count){
+      mostBlogsObj.author = blog.author,
+      mostBlogsObj.count = authorCount;
+    }
+    return mostBlogsObj;
+  },{
+    author: "",
+    count: 0
+  });
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
