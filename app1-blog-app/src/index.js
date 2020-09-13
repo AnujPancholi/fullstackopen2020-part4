178d3836b4135659f0343f54241b794912bcae02
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-13 14:44:53 +0530

    chore: restructure to separate index.js with express app in app.js

--- app1-blog-app/src/index.js
+++ app1-blog-app/src/index.js
@@ -1,37 +1,11 @@
 "use strict";
 
-//env and db connection as early as possible
-require("dotenv").config();
+const app = require("./app.js");
 const CONFIG = require('./utils/config.js');
-const Mongoose = require('mongoose');
+
 
 //logger
 const logger = require('./utils/logger.js');
-
-//db connection
-Mongoose.connect(CONFIG.DB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-}).catch(error => {
-  logger.error(`FATAL: DB CONNECTION ERROR: ${error}`);
-  process.exit(1);
-})
-
-
-//express and related dependencies
-const express = require('express');
-const app = express();
-const cors = require('cors');
-
-//controllers (routes)
-const blogRouter = require('./controllers/blogs.js');
-
-//middlewares
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs',blogRouter);
 
 
 app.listen(CONFIG.PORT, () => {
