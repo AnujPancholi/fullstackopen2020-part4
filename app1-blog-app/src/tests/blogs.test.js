d69c14362348327897826c36b17d77355e9f2dea
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-13 16:25:05 +0530

    test: SUBMISSION - 4.8 -  add test for blogs GET route

---
+++ app1-blog-app/src/tests/blogs.test.js
@@ -0,0 +1,90 @@
+"use strict";
+
+
+const supertest = require("supertest");
+const app = require("../app.js");
+const Mongoose = require("mongoose");
+const BlogModel = require("../models/blogs.js");
+
+
+const API = supertest(app);
+
+
+const TEST_BLOG_DATA = [
+                
+  {
+    "title": "Mock Blog Title",
+    "author": "Author 1",
+    "url": "https://www.mockblog.com/123456",
+    "likes": 23
+  },
+  {
+    "title": "Mock Blog Title 2",
+    "author": "Author 2",
+    "url": "https://www.mockblog.com/1234567",
+    "likes": 544
+  },
+  {
+    "title": "Mock Blog Title 3",
+    "author": "Author 1",
+    "url": "https://www.mockblog.com/1234567",
+    "likes": 8698
+  },
+  {
+    "title": "Mock Blog Title 4",
+    "author": "Author 1",
+    "url": "https://www.mockblog.com/1234567",
+    "likes": 0
+  },
+  {
+    "title": "Mock Blog Title 5",
+    "author": "Author 2",
+    "url": "https://www.mockblog.com/1234567",
+    "likes": 2
+  }
+            
+];
+
+
+beforeEach(() => {
+  return new Promise((resolve,reject) => {
+
+    (async() => {
+      try{
+        await BlogModel.deleteMany({});
+        await Promise.all(TEST_BLOG_DATA.map(testBlogObj => {
+          return (new BlogModel(testBlogObj)).save();
+        }));
+        resolve("TEST DB SET UP");
+      }catch(e){
+        reject(e);
+      }
+    })();
+        
+  })
+},10000)
+
+
+describe("TESTS FOR blogs ROUTE",() => {
+  test("blogs GET should return array of blogs",() => {
+    return new Promise((resolve,reject) => {
+      (async() => {
+        await API.get("/api/blogs").expect((res) => {
+          if(res.status!==200){
+            throw new Error("INCORRECT STATUS CODE");
+          }
+          if(!Array.isArray(res.body) || res.body.length!==TEST_BLOG_DATA.length){
+            throw new Error("INCORRECT NUMBER OF DOCUMENTS RECEIVED");
+          }
+        });
+        resolve(true);
+      })()
+
+    })
+  })
+})
+
+
+afterAll(() => {
+  Mongoose.connection.close();
+})
