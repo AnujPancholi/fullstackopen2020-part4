74d8016418437b25c0cf833dc0dc59df00b1566c
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-09-26 16:15:35 +0530

    feat: add user model and user controller

---
+++ app1-blog-app/src/models/users.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Mongoose = require("mongoose");
+
+
+const userSchema = new Mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  user_type: {
+    type: String,
+    enum: ["ADMIN","STD"],
+    default: "STD"
+  },
+  auth: {
+    hash: {
+      type: String,
+      required: true
+    }
+  }
+})
+
+
+
+const UserModel = new Mongoose.model('User',userSchema);
+
+module.exports = UserModel;
